31026e05f1170a7d28b2888b57e27d05278997fd
Vinjar Vederhus <vinjar97@gmail.com>
2022-12-02 19:37:41 +0100

    Three imported

---
+++ src/index.js
@@ -0,0 +1,29 @@
+import * as THREE from 'three';
+//import { FontLoader } from 'three/addons/loaders/FontLoader.js';
+
+const canvas = document.getElementById('canvas');
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera( 75, canvas.clientWidth / canvas.clientHeight, 0.1, 1000 );
+
+const renderer = new THREE.WebGLRenderer( { canvas: canvas } );
+renderer.setSize( canvas.clientWidth, canvas.clientHeight );
+document.body.appendChild( renderer.domElement );
+
+const geometry = new THREE.BoxGeometry( 1, 1, 1 );
+const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+const cube = new THREE.Mesh( geometry, material );
+scene.add( cube );
+
+camera.position.z = 5;
+
+function animate() {
+    requestAnimationFrame( animate );
+
+    cube.rotation.x += 0.01;
+    cube.rotation.y += 0.01;
+
+    renderer.render( scene, camera );
+};
+
+animate();
