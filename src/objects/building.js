3134e66d82882cfeb6da7add8df7083026cb602c
kristlv1 <kristianlvlv@gmail.com>
2022-12-03 00:00:06 +0100

    hei på deg

--- src/objects/building.js
+++ src/objects/building.js
@@ -1,1 +1,16 @@
 // TODO: yes
+
+class House {
+    constructor(name, scale, translation, rotation) {
+        this.name = name;
+        this.scale = scale;
+        this.translation = translation;
+        this.rotation = rotation;
+
+        this.modelMatrix = new Matrix4();
+        this.modelMatrix.setTranslate(this.translation[0], this.translation[1], this.translation[2]);
+        this.modelMatrix.scale(this.scale[0], this.scale[1], this.scale[2]);
+        this.modelMatrix.rotate(this.rotation, 0, 0, 1);
+    }
+
+}
